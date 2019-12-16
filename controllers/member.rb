affbc6b3f29ef47fbe57135ce16bf7034286b060
Serenity911 <silvia.simonassi01@gmail.com>
2019-12-16 09:59:42 +0000

    added members controller, index show and create

--- controllers/member.rb
+++ controllers/member.rb
@@ -2,3 +2,38 @@ require( 'sinatra' )
 require( 'sinatra/contrib/all' )
 require_relative( '../models/member.rb' )
 also_reload( '../models/*' )
+
+# show all members for a class
+
+# show all members
+get '/members/?' do
+  @members = Member.all()
+  erb(:"members/index")
+end
+
+
+# create new member
+# show form
+get '/members/new' do
+  erb(:"/members/new")
+end
+
+# new member - post
+post '/members' do
+  member = Member.new(params)
+  member.save
+  redirect to("members/")
+end
+
+# show member details
+get '/members/:id' do
+  @member = Member.find(params['id'])
+  erb(:"members/show")
+end
+
+
+
+
+# delete >> archive
+
+# edit a member
