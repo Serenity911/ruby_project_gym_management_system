c0db7292db334b7349e23cee917666c106d8e3f7
Serenity911 <silvia.simonassi01@gmail.com>
2020-02-08 23:04:18 +0000

    edit db name

--- db/sql_runner.rb
+++ db/sql_runner.rb
@@ -1,11 +1,12 @@
 require( 'pg' )
+DATABASE_URL=$(heroku config:get DATABASE_URL -a gym-managemement-app)
 
 class SqlRunner
 
 
   def self.run( sql, values = [] )
     begin
-      db = PG.connect({ dbname: 'gym', host: ENV['DATABASE_URL'] })
+      db = PG.connect({ DATABASE_URL })
       db.prepare("query", sql)
       result = db.exec_prepared( "query", values )
     ensure
